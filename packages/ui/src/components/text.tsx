9ca9e54cdbd34cf53f5eda016c786fac693ad34b
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-05-22 22:16:58 +0530

    feat: add text component

---
+++ packages/ui/src/components/text.tsx
@@ -0,0 +1,7 @@
+export default function Text() {
+  return (
+    <h1 className="h-max w-max px-5 border text-9xl bg-gradient-to-r from-pink-500 to-pink-300 text-transparent bg-clip-text">
+      Hello
+    </h1>
+  );
+}
